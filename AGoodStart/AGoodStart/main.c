cfba125485c01b7c3fef9a6abaa5abcb57c56cd9
Flora Worley <floraworleypdx@gmail.com>
2013-03-08 00:02:11 -0800

    commit AGoodStart

---
+++ AGoodStart/AGoodStart/main.c
@@ -0,0 +1,20 @@
+//
+//  main.c
+//  AGoodStart
+//
+//  Created by Flora Worley on 4/30/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+//Print the beginning of the novel
+    printf("It was the best of times.\n");
+    printf("It was the worst of times.\n");
+    /*Is that actually any good? This may need a rewrite...*/
+    
+    return 0;
+}
+
